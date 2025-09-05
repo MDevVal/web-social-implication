19a1f7abc9aaa03ff8e5490831406b9fe9907d70
chenxiiZ <12czeng@gmail.com>
2025-09-05 13:56:23 -0400

    test

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Social Implication Group Testing deployment - M3GAN
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
